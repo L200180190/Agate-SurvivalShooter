5d9e8bd7c8f1b168e53689b709172fffb409f7e4
Ulin Nuha Trisiana <utrisiana@gmail.com>
2021-10-19 21:39:39 +0700

    Add files via upload

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake ()
+    {
+        // Cari game object dg tag "Player"
+        player = GameObject.FindGameObjectWithTag ("Player");
+
+        // Mendaoat komponen player health
+        playerHealth = player.GetComponent <PlayerHealth> ();
+
+        //enemyHealth = GetComponent<EnemyHealth>();
+
+        // Mendaoat komponen animator 
+        anim = GetComponent <Animator> ();
+    }
+
+    // Callback function jika ada object masuk dalam trigger
+    void OnTriggerEnter (Collider other)
+    {
+        // Set player dlm range
+        if(other.gameObject == player && other.isTrigger == false)
+        {
+            playerInRange = true;
+        }
+    }
+
+    // Callback function jika object keluar dri trigger
+    void OnTriggerExit (Collider other)
+    {
+        // Set player not in range
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
+        {
+            Attack ();
+        }
+
+        // trigger animasi PlayerDead jika darah player <= 0 
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("PlayerDead");
+        }
+    }
+
+
+    void Attack ()
+    {
+        // Reset Timer
+        timer = 0f;
+
+        // Take damage
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    //PlayerShooting playerShooting;
+    bool isDead;                                                
+    bool damaged;
+
+    void Awake()
+    {
+        // Mendapat reference component
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+
+        //playerShooting = GetComponentInChildren<PlayerShooting>();
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        // Apabila terkena damage
+        if (damaged)
+        {
+            // Rubah warna gambar jadi value dari flashColour
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            // Fading out damage image
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed*Time.deltaTime);
+        }
+
+        // Set damage jadi false
+        damaged = false;
+    }
+
+    // Function untuk dapat damage
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        // Mengurangi health
+        currentHealth -= amount;
+
+        // Rubah tampilan heartslider
+        healthSlider.value = currentHealth;
+
+        // Mainkan suara saat kena damage
+        playerAudio.Play();
+
+        // Panggil method Death() jika health <= 0 dan belum mati
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        //playerShooting.DisableEffects();
+
+        // Set Trigger pada animasi Die
+        anim.SetTrigger("Die");
+
+        // Mainkan suara efek saat mati
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        // Matikan script PlayerMovement
+        playerMovement.enabled = false;
+
+        //playerShooting.enabled = false;
+    }
+
+    public void RestartLevel()
+    {
+        // Load ulang scene dg index 0 pada build setting
+        SceneManager.LoadScene(0);
+    }
+}
